2e27f144a440c2569aa372b4357d7ffe33e32b3f
Filippi Sgreccia <ffss2006bolado@hotmail.com>
2021-06-02 10:07:19 -0300

    app finalizado

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+  
+import { useMovies } from '../hooks/useMovies';
+
+import '../styles/header.scss';
+
+export function Header() {
+  const {
+    selectedGenre,
+  } = useMovies();
+
+  return (
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+
+
 import { render } from 'react-dom'
 
 import { App } from './App'
